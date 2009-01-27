b49370ea9eaea015f836521be15cf225cb5fa0dd
Simon Roe <simon.roe@talusdesign.co.uk>
2009-01-27 21:52:11 +0000

    Added country codes :)

---
+++ indexer/countryCodes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+def countryCodes():
+  codes = []
+  codes.append('AT')
+  codes.append('BE')
+  codes.append('BG')
+  codes.append('CZ')
+  codes.append('AT')
+  codes.append('EE')
+  codes.append('FR')
+  codes.append('DE')
+  codes.append('HU')
+  codes.append('IE')
+  codes.append('IT')
+  codes.append('LV')
+  codes.append('LT')
+  codes.append('PL')
+  codes.append('PT')
+  codes.append('S')  
+  codes.append('SK')
+  codes.append('SL')
+  codes.append('ES')
+  codes.append('UK')
+  return codes
+  
+  
+if __name__ == '__main__':
+  print countryCodes()
+
