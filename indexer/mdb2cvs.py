a568e684cdf77b687bc973a6aedb2e07512f4d92
Simon Roe <simon.roe@talusdesign.co.uk>
2009-02-03 20:53:13 +0000

    Moved filenameToCountryCode to countryCodes

--- indexer/mdb2cvs.py
+++ indexer/mdb2cvs.py
@@ -23,7 +23,7 @@ def extractmdb2csv(countryToProcess="all"):
     filename = db.split('/')[-1]
 
     # Get the country
-    country = filenameToCountryCode(filename)
+    country = countryCodes.filenameToCountryCode(filename)
     if countryToProcess is not "all" and countryToProcess != country:
       continue
     
@@ -60,50 +60,7 @@ def extractmdb2csv(countryToProcess="all"):
         file_all.write(fields+"\n")
         file_all.close()
         
-def filenameToCountryCode(filename):
-  if filename[0:7] == 'austria':
-    return 'AT'
-  if filename[0:7] == 'belgium':
-    return 'BE'
-  if filename[0:8] == 'bulgaria':
-    return 'BG'
-  if filename[0:5] == 'czech':
-    return 'CZ'
-  if filename[0:7] == 'estonia':
-    return 'AT'
-  if filename[0:7] == 'finland':
-    return 'EE'
-  if filename[0:6] == 'france':
-    return 'FR'
-  if filename[0:7] == 'germany':
-    return 'DE'
-  if filename[0:7] == 'hungary':
-    return 'HU'
-  if filename[0:7] == 'ireland':
-    return 'IE'
-  if filename[0:5] == 'italy':
-    return 'IT'
-  if filename[0:6] == 'latvia':
-    return 'LV'
-  if filename[0:9] == 'lithuania':
-    return 'LT'
-  if filename[0:10] == 'netherland':
-    return 'NL'        
-  if filename[0:6] == 'poland':
-    return 'PL'
-  if filename[0:8] == 'portugal':
-    return 'PT'
-  if filename[0:8] == 'slovakia':
-    return 'SK'
-  if filename[0:8] == 'slovenia':
-    return 'SL'
-  if filename[0:5] == 'spain':
-    return 'ES'
-  if filename[0:13] == 'unitedkingdom':
-    return 'UK'
-  else:
-    raise Exception, "Could not work out country for %s!" % filename
-  
+
   
   
   
