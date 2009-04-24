1e61812130d3cd03265ee9bdbf6091f57391fc99
Simon Roe <simon.roe@talusdesign.co.uk>
2009-04-24 13:53:06 +0100

    * Fixed more graphlib bugs

--- web/graphs/graphlib.py
+++ web/graphs/graphlib.py
@@ -24,31 +24,29 @@ def make_fig(request, type):
     name_value_dict = {}
     
     years = sorted(request.GET)
+    name_value_dict = []
     # assert False
     for year in years:
-      name_value_dict[year] = float(request.GET[year])
+      name_value_dict.append((year,float(request.GET[year])))
     # assert False
-    # name_value_dict = sorted(name_value_dict.keys())
-    
-    # import sys
-    # sys.exit(name_value_dict)
+
     
     figure(figsize=(5, 2), linewidth=0) # image dimensions  
-    # title(graph_title, size='x-small')
+
     subplots_adjust(left=0.2)
-    # autofmt_xdate()
-    
-    
+
     # add bars
-    for i, key in enumerate(name_value_dict.keys()):
-        bar(i + 0.25 , name_value_dict[key], 0.5,  color='grey', alpha=0.7, linewidth=0)
+    i = 0
+    for key in name_value_dict:
+      bar(i+0.25 , key[1], 0.5,  color='grey', alpha=0.7, linewidth=0)
+      i = i+1
 
     # axis setup
     xticks(arange(0.5, len(name_value_dict)),
-        [('%s' % name) 
-        for name, value in name_value_dict.items()],
+        [('%s' % value[0]) 
+        for value in name_value_dict],
         size='xx-small')
-    max_value = max(name_value_dict.values())
+    max_value = max([v[1] for v in name_value_dict])
     tick_range = arange(0, max_value+max_value*1.1, round(max_value,1)/2)
     yticks(tick_range, size='xx-small')
     formatter = FixedFormatter([u"\u20ac%s" % locale.format(u'%d', float(x), True) for x in tick_range])
