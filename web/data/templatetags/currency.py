a52a95ff8cd5d782545ec8b652f5356c5e86cf4f
Simon Roe <simon.roe@talusdesign.co.uk>
2009-06-03 18:58:32 +0100

    various fixes

--- web/data/templatetags/currency.py
+++ web/data/templatetags/currency.py
@@ -2,11 +2,12 @@ from django import template
 import locale
 locale.setlocale(locale.LC_ALL, '')
 register = template.Library()
- 
+from django.contrib.humanize.templatetags import humanize
  
 @register.filter()
 def currency(value):
   try:
+    # decimal = value.split('.')[1]
     return locale.format('%.2f', float(value), True)
   except:
     return value
