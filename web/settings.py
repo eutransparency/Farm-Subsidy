8afbc35ec88221ac5271d57e8be54a763a159aa8
Simon Roe <simon.roe@talusdesign.co.uk>
2009-08-31 21:33:36 +0100

    Merge new theme in to 2.0 branch

--- web/settings.py
+++ web/settings.py
@@ -1,6 +1,6 @@
 # Django settings for farmjango project.
 
-DEBUG = True 
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'farmjango'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_USER = 'farmsubsidy_writer'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'netopia'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
