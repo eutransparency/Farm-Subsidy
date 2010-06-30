7ba303fb0ef8b92d11b3b14ed6556922c2d0617d
Sym Roe <sym.roe@talusdesign.co.uk>
2010-06-30 13:12:03 +0100

    Remove old custom list attempts

--- web/customlists/context_processors.py
+++
@@ -1,7 +0,0 @@
-def custom_list(request):
-  custom_list = {}
-  if request.session.get('create_list', False):
-    custom_list['active'] = True
-    custom_list['list_items'] = request.session.get('custom_list_items', {})
-    custom_list['total'] = request.session.get('custom_list_items_total', 0)    
-  return {'custom_list' : custom_list}
