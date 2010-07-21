c6d1bc0b2ce63b5ea004f4097634fcf7b3f91144
Richard <richard@memespring.co.uk>
2010-07-21 14:58:28 +0100

    extra header images

--- web/misc/context_processors.py
+++ web/misc/context_processors.py
@@ -26,5 +26,5 @@ def google_api_key(request):
     return {'GOOGLE_MAPS_API_KEY' : settings.GOOGLE_MAPS_API_KEY}
     
 def header_class(request):
-    return {'header_class' : 'header_' + str(random.randint(1,8))}  
+    return {'header_class' : 'header_' + str(random.randint(1,10))}  
 
