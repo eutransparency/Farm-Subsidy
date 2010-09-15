03a30178e4e7084a3235ad07314634b8811fb02b
Sym Roe <sym.roe@talusdesign.co.uk>
2010-09-15 12:38:50 +0100

    Map on transparency list page

--- web/countryinfo/views.py
+++ web/countryinfo/views.py
@@ -19,8 +19,28 @@ def compare(request):
 def transparency_list(request):
     transparency_list = TransparencyScore.objects.all().order_by('rank')
     
+    green = "00D400"
+    yellow = "FF6600"
+    red = "FE0000"
+    
+    map_colours = []
+    map_countries = []
+    for country in transparency_list:
+        map_countries.append(country.country)
+        if country.score > 50:
+            map_colours.append(green)
+        elif country.score > 30:
+            map_colours.append(yellow)
+        else:
+            map_colours.append(red)
+
+    
     return render_to_response(
         'transparency_list.html', 
-        {'list' : transparency_list, },
+        {
+            'list' : transparency_list, 
+            'map_colours' : map_colours, 
+            'map_countries' : map_countries, 
+        },
         context_instance=RequestContext(request)
     )
