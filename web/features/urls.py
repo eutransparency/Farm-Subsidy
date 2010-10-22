b6e9b7ac5c97a000fc04721418a0c3a81e4f58d6
Sym Roe <sym.roe@talusdesign.co.uk>
2010-10-22 22:14:10 +0100

    Link to google maps on recipient page

--- web/features/urls.py
+++ web/features/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
+from feeds import FeaturesFeed
 import views
 
 urlpatterns = patterns('',
    url(r'^$', views.feature_list, name="feature_list"),
+   url(r'^feed/$', FeaturesFeed(), name="feature_feed"),
    url(r'^(?P<slug>[-\w]+)/$', views.feature_detail, name="feature_detail"),
    )
